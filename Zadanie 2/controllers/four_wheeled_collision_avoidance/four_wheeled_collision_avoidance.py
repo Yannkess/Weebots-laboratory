0ece5c062f60c0eb0078b7b5ec285c339b436265
Yannkess <67194530+Yannkess@users.noreply.github.com>
2022-12-24 00:04:12 +0100

    1.2

--- Zadanie 2/controllers/four_wheeled_collision_avoidance/four_wheeled_collision_avoidance.py
+++ Zadanie 2/controllers/four_wheeled_collision_avoidance/four_wheeled_collision_avoidance.py
@@ -1,10 +1,9 @@
 from controller import Robot
-
+detected = 0
 TIME_STEP = 64
 robot = Robot()
 ds = []
 dsNames = ['ds_right', 'ds_left','colorSensor']
-j = 2
 for i in range(3):
     ds.append(robot.getDevice(dsNames[i]))
     ds[i].enable(TIME_STEP)
@@ -16,19 +15,36 @@ for i in range(4):
     wheels[i].setVelocity(0.0)
 avoidObstacleCounter = 0
 while robot.step(TIME_STEP) != -1:
-    leftSpeed = 1.0
-    rightSpeed = 1.0
-    if avoidObstacleCounter > 0:
-        avoidObstacleCounter -= 1
-        leftSpeed = 1.0
-        rightSpeed = -1.0
-    else:  # read sensors
-       for i in range(2):   
-            if ds[i].getValue() < 950.0 :
-                avoidObstacleCounter = 0   
-    if ds[j].getValue() > 900.0 :
-        avoidObstacleCounter = 100 
-        print(ds[j].getValue())      
+    leftSpeed = 2.0
+    rightSpeed = 2.0
+    leftds = ds[0].getValue()
+    rightds = ds[1].getValue()
+    colSens = ds[2].getValue()
+    if detected == 1:
+        print("left ds = " + str(leftds))
+        print("right ds = " + str(rightds))
+        if leftds >= 1000 and rightds >= 500:
+            leftSpeed = -2
+            rightSpeed = 2
+        elif rightds >= 1000 and leftds >= 500:
+            leftSpeed = 2
+            rightSpeed = -2
+        else:
+            leftSpeed = 2.0
+            rightSpeed = 2.0
+    elif detected == 0:
+        if avoidObstacleCounter > 0:
+            avoidObstacleCounter -= 1
+            leftSpeed = -1.0
+            rightSpeed = 1.0     
+        else:  # read sensors
+            for i in range(2):   
+                    if ds[i].getValue() < 550.0 :
+                        detected = 1                   
+    if colSens > 900.0:
+          avoidObstacleCounter = 100 
+          detected = 0
+    print("Detected = " + str(detected))
     wheels[0].setVelocity(leftSpeed)
     wheels[1].setVelocity(rightSpeed)
     wheels[2].setVelocity(leftSpeed)
